55087102c07115d6e69a3630da40061834ba755f
marcprux <marc@glimpse.io>
2021-06-22 15:06:36 -0400

    chore: checkpoint

--- Package.swift
+++ Package.swift
@@ -2,20 +2,23 @@
 
 import PackageDescription
 
-#if os(Linux)
-let targets: [Target] = [
-    .systemLibrary(name: "CJSCore", pkgConfig: "javascriptcoregtk-4.0", providers: [.apt(["libjavascriptcoregtk-4.0-dev"])]),
-    .target(name: "JXKit", dependencies: ["CJSCore"]),
-    .testTarget(name: "JXKitTests", dependencies: ["JXKit"])
-]
-#else
+#if canImport(JavaScriptCore)
 let targets: [Target] = [
     .target(name: "JXKit"),
-    .testTarget(name: "JXKitTests", dependencies: ["JXKit"])
+    .testTarget(name: "JXKitTests", 
+        dependencies: ["JXKit"])
+]
+#else // no native JavaScriptCore falls back to javascriptcoregtk
+let targets: [Target] = [
+    .systemLibrary(name: "CJSCore", 
+        pkgConfig: "javascriptcoregtk-4.0", 
+        providers: [.apt(["libjavascriptcoregtk-4.0-dev"])]),
+    .target(name: "JXKit", 
+        dependencies: ["CJSCore"]),
+    .testTarget(name: "JXKitTests", 
+        dependencies: ["JXKit"])
 ]
 #endif
-
-//.unsafeFlags(["-I/usr/include/webkitgtk-4.0"])
 
 let package = Package(
     name: "JXKit",
