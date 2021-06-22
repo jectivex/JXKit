1c079b49caf64c036543ef4f775e2bb6aeed444f
marcprux <marc@glimpse.io>
2021-06-22 14:35:22 -0400

    chore: checkpoint

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let targets: [Target] = [
 #else
 let targets: [Target] = [
     .systemLibrary(name: "CJSCore", pkgConfig: "javascriptcoregtk", providers: [.brew(["libjavascriptcoregtk-4.0-dev"]), .apt(["libjavascriptcoregtk-4.0-dev"])]),
-    .target(name: "JXKit", dependencies: ["CJSCore"], cSettings: [.define("_GNU_SOURCE", to: "1")]),
+    .target(name: "JXKit", dependencies: ["CJSCore"], cSettings: [ .unsafeFlags(["-I/usr/include/webkitgtk-4.0"]) ]),
     .testTarget(name: "JXKitTests", dependencies: ["JXKit"])
 ]
 #endif
