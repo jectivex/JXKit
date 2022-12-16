64eb9e4e373bd588346c1d9d6dfff9545dbac475
marcprux <mwp1@cornell.edu>
2022-12-16 18:22:21 -0500

    Fix test to reflect Intl behavior differences per platform

--- Tests/JXKitTests/JXKitTests.swift
+++ Tests/JXKitTests/JXKitTests.swift
@@ -45,8 +45,15 @@ final class JXKitTests: XCTestCase {
         let jxc = JXContext()
 
         XCTAssertEqual("12,34 €", try jxc.eval("new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(12.34)").string)
-        XCTAssertEqual("¥46", try jxc.eval("new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(45.678)").string)
         XCTAssertEqual("65.4", try jxc.eval("new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(65.4321)").string)
+
+        let yen = "new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(45.678)"
+        // I'm guessing these are different values because they use combining marks differently
+        #if os(Linux)
+        XCTAssertEqual("￥46", try jxc.eval(yen).string)
+        #else
+        XCTAssertEqual("¥46", try jxc.eval(yen).string)
+        #endif
 
         XCTAssertEqual("10/24/2022", try jxc.eval("new Intl.DateTimeFormat('en-US', {timeZone: 'UTC'}).format(new Date('2022-10-24'))").string)
         XCTAssertEqual("24/10/2022", try jxc.eval("new Intl.DateTimeFormat('fr-FR', {timeZone: 'UTC'}).format(new Date('2022-10-24'))").string)
